f0f3204e6a796d50156310d292caff798e93a898
BEERXD0000 <163150702+BEERXD0000@users.noreply.github.com>
2024-05-12 13:38:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ client.on('ready', async () => {
   setInterval(() => {
     const moment = require('moment');
     const created = moment().format('YYYY-MM-DD HH:mm:ss ');
-    const change = ['https://cdn.discordapp.com/attachments/1227927155732774995/1237055048962412585/IMG_0629.jpg?ex=66402f8b&is=663ede0b&hm=be5eb337157d37a268e75937400eeacf8d34dc96c329f553d4c4b6bc5c23b554&']; //รูปใหญ่
+    const change = ['https://cdn.discordapp.com/attachments/1227927155732774995/1235232657324114200/F39270F5-7FFF-4922-B9F7-854C14559872.gif?ex=6641774f&is=664025cf&hm=ab7967ed2f37dd59e0acab95e273ddc9c837c7eaec9cc77ff229bd573fd24cf3&']; //รูปใหญ่
      //const poop = ['https://cdn.discordapp.com/emojis/1152953111778967613.webp?size=96&quality=lossless']; // รูปเล็ก
      //const change2 = ['']; // ชื่อสลับ
     const yyyt = ['⪩ kungYuna']; // ชื่อ button2
